3d882de3a0715b548892ac6e38c32d11f7d1d066
Dave Nicholson <fatnic388@gmail.com>
2016-06-01 21:11:10 +0100

    added some keycodes

--- js/libs/keycontrol.js
+++ js/libs/keycontrol.js
@@ -1,16 +1,25 @@
 var Key = {
     _pressed: {},
 
+    ONE: 49,
+    TWO: 50,
+    THREE: 51,
+    FOUR: 52,
+    FIVE: 53,
+    SIX: 54,
+    SEVEN: 55,
+    EIGHT: 56,
+    NINE: 57,
+    ZERO: 48,
+
     SPACE: 32,
+    ENTER: 13,
+    ESC: 27,
 
-    // Dynamically build this
-
-    A: 65,
-    B: 66,
-    C: 68,
-    W: 87,
-    D: 68,
-    S: 83,
+    UP: 38,
+    DOWN: 40,
+    LEFT: 37,
+    RIGHT: 39,
 
     LSqrBrkt: 219,
     RSqrBrkt: 221,
@@ -20,9 +29,13 @@ var Key = {
     onKeyup: function(event) { delete this._pressed[event.keyCode]; }
 };
 
+// Build codes for letter keys
+for(var i=65; i<91; i++) { Key[String.fromCharCode(i)] = i; }
+
 window.addEventListener('keyup', function(event) {
     Key.onKeyup(event);
 }, false);
+
 window.addEventListener('keydown', function(event) {
     Key.onKeydown(event);
 }, false);
