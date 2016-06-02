36ae80182c5f2c74bce9aa8d91a8df4eb127d766
Dave Nicholson <fatnic388@gmail.com>
2016-06-02 15:23:30 +0100

    added a rect function

--- js/libs/drawtools.js
+++ js/libs/drawtools.js
@@ -1,5 +1,19 @@
 var DrawTools = {
 
+    line: function(context, start, end, colour, thickness) {
+        context.strokeStyle = (typeof(colour) === 'undefined') ? 'white' : thickness;
+        context.lineWidth = (typeof(thickness) === 'undefined') ? 1 : thickness;
+        context.beginPath();
+        context.moveTo(start.x, start.y);
+        context.lineTo(end.x, end.y);
+        context.stroke();
+    },
+
+    fillRect: function(context, position, size, colour) {
+        context.fillStyle = (typeof(colour) === 'undefined') ? 'white' : colour;
+        context.fillRect(position.x, position.y, size.x, size.y);
+    },
+
     circle: function(context, position, radius, colour) {
         context.fillStyle = colour;
         context.beginPath();
@@ -9,15 +23,15 @@ var DrawTools = {
 
     ring: function(context, position, radius, colour, thickness) {
         context.strokeStyle = colour;
-        context.lineWidth = (typeof(thickness) === 'undefined') ? 2 : thickness;
+        context.lineWidth = (typeof(thickness) === 'undefined') ? 1 : thickness;
         context.beginPath();
     	context.arc(position.x, position.y, radius, 0, RAD, false);
         context.stroke();
     },
 
     ringedCircle: function(context, position, radius, circleColour, ringColour, thickness) {
-        this.circle(position, radius, circleColour);
-        this.ring(position, radius, ringColour, thickness);
+        this.circle(context, position, radius, circleColour);
+        this.ring(context, position, radius, ringColour, thickness);
     },
 
     polygon: function(context, polygon, colour) {
