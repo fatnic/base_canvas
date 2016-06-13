4ed2de5d547cea952814662f07c94c62c29173a5
Dave Nicholson <fatnic388@gmail.com>
2016-06-13 20:51:41 +0100

    i prefer seperate prototype calls

--- js/libs/assetmanager.js
+++ js/libs/assetmanager.js
@@ -12,7 +12,7 @@
     AM.get('json.loader');
 */
 
-function AssetManager(config){
+function AssetManager(config) {
     this.successCount = 0;
     this.errorCount = 0;
     this.downloadQueue = [];
@@ -22,74 +22,84 @@ function AssetManager(config){
     this.init();
 }
 
-AssetManager.prototype = {
+AssetManager.prototype.init = function() {
+    for (var i = 0; i < this.config.paths.length; i++) {
+        this.queueDownload(this.config.paths[i]);
+    }
+};
 
-    init: function() {
-        for (var i=0; i < this.config.paths.length; i++) {
-            this.queueDownload(this.config.paths[i]);
+AssetManager.prototype.queueDownload = function(path) {
+    this.downloadQueue.push(this.config.base + path);
+};
+
+AssetManager.prototype.downloadAll = function(callback) {
+
+    if (this.downloadQueue.length === 0) callback();
+
+    for (var i = 0; i < this.downloadQueue.length; i++) {
+        var path = this.downloadQueue[i];
+        var ext = path.split('.').pop();
+
+        switch (ext) {
+            case 'png':
+            case 'jpg':
+            case 'jpeg':
+                this.loadImage(path, callback);
+                break;
+            case 'json':
+                this.loadJSON(path, callback);
+                break;
+            default:
+                console.log('AssetManager: unknown filetype');
+                break;
         }
-    },
+    }
+};
 
-    queueDownload: function(path) { this.downloadQueue.push(this.config.base + path); },
+AssetManager.prototype.isDone = function() {
+    return (this.downloadQueue.length == this.successCount + this.errorCount);
+};
 
-    downloadAll: function(callback) {
+AssetManager.prototype.get = function(key) {
+    return this.cache[key];
+};
 
-        if (this.downloadQueue.length === 0) callback();
-
-        for (var i=0; i<this.downloadQueue.length;i++) {
-            var path = this.downloadQueue[i];
-            var ext = path.split('.').pop();
-
-            switch (ext) {
-                case 'png':
-                case 'jpg':
-                case 'jpeg':
-                    this.loadImage(path, callback);
-                    break;
-                case 'json':
-                    this.loadJSON(path, callback);
-                    break;
-                default:
-                    console.log('AssetManager: unknown filetype');
-                    break;
-            }
+AssetManager.prototype.loadImage = function(path, callback) {
+    var img = new Image();
+    var that = this;
+    img.addEventListener("load", function() {
+        that.successCount += 1;
+        if (that.isDone()) {
+            callback();
         }
-    },
+    }, false);
+    img.addEventListener("error", function() {
+        that.errorCount += 1;
+        if (that.isDone()) {
+            callback();
+        }
+    }, false);
+    img.src = path;
+    this.cache["img." + this.makeKey(path)] = img;
+};
 
-    isDone: function() { return (this.downloadQueue.length == this.successCount + this.errorCount); },
-
-    get: function(key) { return this.cache[key]; },
-
-    loadImage: function(path, callback) {
-        var img = new Image();
-        var that = this;
-        img.addEventListener("load", function(){
+AssetManager.prototype.loadJSON = function(path, callback) {
+    var that = this;
+    var xhr = new XMLHttpRequest();
+    xhr.overrideMimeType('application/json');
+    xhr.open('GET', path, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == "200") {
+            that.cache["json." + that.makeKey(path)] = JSON.parse(xhr.responseText);
             that.successCount += 1;
-            if(that.isDone()) { callback(); }
-        }, false);
-        img.addEventListener("error", function(){
-            that.errorCount += 1;
-            if(that.isDone()) { callback(); }
-        }, false);
-        img.src = path;
-        this.cache["img." + this.makeKey(path)] = img;
-    },
-
-    loadJSON: function(path, callback) {
-        var that = this;
-        var xhr = new XMLHttpRequest();
-        xhr.overrideMimeType('application/json');
-        xhr.open('GET', path, true);
-        xhr.onreadystatechange = function(){
-            if(xhr.readyState == 4 && xhr.status == "200"){
-                that.cache["json." + that.makeKey(path)] = JSON.parse(xhr.responseText);
-                that.successCount += 1;
-                if(that.isDone()) { callback(); }
+            if (that.isDone()) {
+                callback();
             }
-        };
-        xhr.send(null);
-    },
+        }
+    };
+    xhr.send(null);
+};
 
-    makeKey: function(path) { return path.split('/').pop().split('.')[0]; }
-
+AssetManager.prototype.makeKey = function(path) {
+    return path.split('/').pop().split('.')[0];
 };
